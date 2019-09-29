f9857f32ccc299f880e0e1e758cf78edf1fbd5f7
DioSchadner <55942345+DioSchadner@users.noreply.github.com>
2019-09-29 10:34:36 +0200

    Update and rename testfile.txt to HelloVisualStudio.cpp

---
+++ ILV01/HelloVisualStudio.cpp
@@ -0,0 +1,12 @@
+// HelloVisualStudio.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "conio.h"
+
+
+void main()
+{
+	printf("Hello VisualStudio!\n");
+	_getch();
+}
